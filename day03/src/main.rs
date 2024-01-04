20160a6324fb1b52a783198879d230e92263b5f9
Peter Cox <Peter.Cox@kingfisher.com>
2024-01-04 11:31:08 +0000

    Day03 (not complete yet)

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,3 +1,52 @@
+use std::{fs::File, io::{BufReader, BufRead}};
+
 fn main() {
-    println!("Hello, world!");
+    let f = File::open(concat!(env!("CARGO_MANIFEST_DIR"), "\\input.txt")).unwrap();
+    let f = BufReader::new(f);
+
+    //let mut number_of_ones: [i32; 15] = [0; 15];
+    //let mut number_of_zeros: [i32; 15] = [0; 15];
+    
+    let lines: Vec<u32> = f
+                            .lines()
+                            .map(|n| u32::from_str_radix(&n.unwrap(), 2).unwrap())
+                            .collect();
+
+
+    let mut g: u32 = 0;
+    let mut e: u32 = 0;
+    for i in 0..11 {
+        for x in &lines {
+            println!("{x}");
+        }
+    }
+/*    for line in f.lines() {
+        let buf = line.unwrap();
+        let buf_r = buf.as_bytes();
+        let mut counter = 0;
+        for char in buf_r {
+            if *char == 48 as u8 {
+                number_of_zeros[counter] += 1;
+            }
+            else {
+                number_of_ones[counter] += 1;
+            }
+            counter += 1;
+            if counter > max_counter { max_counter = counter; }
+        }
+    }
+
+    for i in 0..max_counter {
+       g *= 2;
+       e *= 2;
+       if number_of_ones[i] > number_of_zeros[i] {
+            g += 1;
+       }
+       else {
+           e += 1;
+       }
+    }
+    
+    println!("Part 1: {} * {}", g, e);
+     */
 }
